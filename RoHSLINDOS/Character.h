3603fcb5659aa522dcfe13b764a09c770501c899
seventyfive75 <acikeyupcan@gmail.com>
2025-05-15 16:43:38 +0300

    character.h ve character.cpp dosyaları oluşturuldu. gerekli tanımlamalar yapıldı, gerekli eklemeler yapıldı. position ve camera clamplenmesi tick fonksiyonunda mevcut. draw ve update animation fonksiyonu var.

--- RoHSLINDOS/Character.h
+++ RoHSLINDOS/Character.h
@@ -1,1 +1,38 @@
+#pragma once
+#include "raylib.h"
+#include "raymath.h"
 
+class Character {
+public:
+    Character(Vector2 pos, float s, int screenWidth, int screenHeight, int mapWidth, int mapHeight);
+
+    void tick(float deltaTime, Camera2D& camera);
+    void draw();
+
+    Vector2 getPosition() const;
+
+private:
+    Texture2D knight_idle;
+    Texture2D knight_run;
+    Texture2D currentTexture;
+
+    Vector2 position{};
+    float moveX{}, moveY{};
+    float speed;
+
+    float currentSpeedX{}, currentSpeedY{};
+    float targetSpeedX{}, targetSpeedY{};
+
+    float facingDirection{ 1.0f };
+
+    int frame{};
+    float runningTime{};
+    const int maxFrames{ 6 };
+    const float updateTime{ 1.f / 12.f };
+
+    int screenW, screenH;
+    int mapW, mapH;
+
+    void updateMovement(float deltaTime);
+    void updateAnimation(float deltaTime);
+};
--- RoHSLINDOS/main.cpp
+++ RoHSLINDOS/main.cpp
@@ -5,38 +5,20 @@
 int main()
 {
     const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
-    const int mapWidth = 3050, mapHeight = 4050;
-    const float playerSpeed = 500.0f;
-    float targetSpeedX{};
-    float targetSpeedY{};
-    float currentSpeedX = 0.0f;
-    float currentSpeedY = 0.0f;
-    float deltaTime{};
-    float moveX{};
-    float moveY{};
-    float runningTime{};
-    int frame{};
-    const int maxFrames{ 6 };
-    const float updateTime{ 1.f / 12.f };
-    float facingDirection{ 1.0f }; //default sað
-
+    const int mapWidth = 3060, mapHeight = 3060;
+    const float playerSpeed = 5000.0f;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib Game");
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos{ 0.0, 0.0 };
 
-    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png"); //durma
-    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png"); //koþma
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png"); //ana tex
-    Vector2 knightPos{
-        (float)SCREEN_WIDTH / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
-        (float)SCREEN_HEIGHT / 2.0f - 4.0f * (0.5f * (float)knight.height)
-    };
+    Character knight({ SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f }, playerSpeed,
+                 SCREEN_WIDTH, SCREEN_HEIGHT, mapWidth, mapHeight);
 
     Camera2D camera{
         {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f} ,
-         knightPos,
+         knight.getPosition(),
          0.0f,
          1.0f
     };
@@ -45,60 +27,18 @@ int main()
     while (!WindowShouldClose())
     {
         //INPUT ALIM KISMI
-        moveX = IsKeyDown(KEY_D) - IsKeyDown(KEY_A); // -1, 0 veya 1
-        moveY = IsKeyDown(KEY_S) - IsKeyDown(KEY_W); // -1, 0 veya 1
 
         //UPDATE KISMI
-        deltaTime = GetFrameTime();
+        float deltaTime = GetFrameTime();
 
-        //Çapraz daha hýzlý gitmeyi önleme
-        Vector2 moveDir = { moveX, moveY };
-        if (Vector2Length(moveDir) > 0) {
-            moveDir = Vector2Normalize(moveDir);
-        }
+        camera.target = knight.getPosition();
 
-        //hareket için targetSpeed belirleme
-        targetSpeedX = moveDir.x * playerSpeed;
-        targetSpeedY = moveDir.y * playerSpeed;
-
-        //ivmeli hareket için lerp
-        currentSpeedX = Lerp(currentSpeedX, targetSpeedX, 10.0f * deltaTime);
-        currentSpeedY = Lerp(currentSpeedY, targetSpeedY, 10.0f * deltaTime);
-
-        //hareket update i 
-        knightPos.x += currentSpeedX * deltaTime;
-        knightPos.y += currentSpeedY * deltaTime;
+        knight.tick(deltaTime, camera);
 
         //kamera update
-        camera.target = knightPos;
+        
 
-        //sýnýrlar içerisinde ise clample
-        if (mapWidth > SCREEN_WIDTH)
-        {
-            camera.target.x = Clamp(knightPos.x, SCREEN_WIDTH / 2.0f, mapWidth - SCREEN_WIDTH / 2.0f);
-        }
-        if (mapHeight > SCREEN_HEIGHT)
-        {
-            camera.target.y = Clamp(knightPos.y, SCREEN_HEIGHT / 2.0f, mapWidth - SCREEN_HEIGHT / 2.0f);
-
-        }
-
-        //belki kamera için de ileride lerp???
-
-        //karakter duruyor mu koþuyor mu
-        knight = (moveX != 0.0f || moveY != 0.0f) ? knight_run : knight_idle;
-
-        //facing direction update
-        if (moveX != 0) facingDirection = (moveX > 0) ? 1.0f : -1.0f;
-
-        // sprite sheet i güncelleme
-        runningTime += deltaTime;
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
-        }
+        
 
         //ÇÝZÝM KISMI
         BeginDrawing();
@@ -109,18 +49,11 @@ int main()
         //harita çizimi
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        //karakter çizimi
-        Rectangle source{ frame * (float)knight.width / 6.f, 0.f, facingDirection * (float)knight.width / 6.f, (float)knight.height };
-        Rectangle dest{ knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height };
-        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+        knight.draw();
 
         EndMode2D();
         EndDrawing();
     }
-
-    UnloadTexture(knight);
-    UnloadTexture(knight_idle);
-    UnloadTexture(knight_run);
     UnloadTexture(map);
     CloseWindow();
 
